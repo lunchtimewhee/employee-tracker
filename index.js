07aea841a3f9643259ae3834cee596434cb50ab1
Anthony Li <anthonyrli1994@gmail.com>
2023-01-31 22:54:56 -0500

    implemented create employee

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const { create } = require('./models/Department');
 const { json } = require('body-parser');
 
 let activeDepartmentList = [];
-
+let activeRoleList = [];
+let activeEmployeeList = [];
+let display = true;
 
 
 
@@ -39,8 +41,8 @@ const askQuestions = async function () {
             case 'Add Role':
                 await createRole(answers.roleName, answers.roleSalary, answers.roleDepartment);
                 break;
-            case 'View All Employees':
-                await getEmployee();
+            case 'Add Employee':
+                await createEmployee(answers.employeeFirstName, answers.employeeLastName, answers.employeeRole, answers.employeeManager);
                 break;
             case 'View All Employees':
                 await getEmployee();
@@ -99,29 +101,60 @@ const employeeQuestions = [
 // Sync tables
 const syncTables = async function() {
     await sequelize.sync();
-}
+};
 
+// Return a list of all departments
 const getDepartment = async function() {
     const departmentList = await Department.findAll({
         raw: true,
     });
 
-    // Create a list of departments that will be returned to be used in the inquirer prompt
+    if(display){
+        console.table(departmentList);
+    }
+
+    // Set a value for each department = to its id. Inquirer will use this value as the answer. 
     departmentList.forEach((dep) => {
         dep.value = dep.id;
     });
 
     activeDepartmentList = departmentList;
-}
+    
+};
 
+// Create a new department and add it to the database
 const createDepartment = async function(newName) {
     const newDepartment = await Department.create({
         name: newName,
     })
 
     console.log(`\nAdded ${newName} to the database. \n`)
-}
+};
 
+
+// Return a list of all roles
+const getRole = async function() {
+    const roleList = await Role.findAll({
+        raw: true,
+    });
+
+    if(display){
+        console.table(roleList);
+    }
+    
+
+    // Set a value for each role = to its id. Inquirer will use this value as the answer. 
+    roleList.forEach((role) => {
+        role.value = role.id;
+        role.name = role.title;
+    });
+
+    activeRoleList = roleList;
+    
+};
+
+
+// Create a new Role and add it to the database
 const createRole = async function(newTitle, newSalary, newDepartmentId) {
     const newRole = await Role.create({
         title: newTitle,
@@ -130,41 +163,41 @@ const createRole = async function(newTitle, newSalary, newDepartmentId) {
     })
 
     console.log(`\nAdded ${newTitle} to the database. \n`)
-}
+};
 
-const getRole = async function() {
-    const roleList = await Role.findAll({
-        raw: true,
-    });
-
-    console.table(roleList);
-}
-
-const createEmployee = async function() {
-    const newRole = await Employee.create({
-        first_name: 'bob',
-        last_name: 'bill',
-        role_id: 1,
-
-    })
-}
-
-const createManager = async function() {
-    const newRole = await Employee.create({
-        first_name: 'sally',
-        last_name: 'bill',
-        role_id: 1,
-        manager_id: 3
-    })
-}
-
+// Return a list of all employees
 const getEmployee = async function() {
     const employeeList = await Employee.findAll({
         raw:true
     });
 
-    console.table(employeeList);
+    if(display){
+        console.table(employeeList);
+    }
+    
+     // Set a value for each Employee = to its id. Inquirer will use this value as the answer. 
+     employeeList.forEach((e) => {
+        e.value = e.id;
+        e.name = `${e.first_name} ${e.last_name}`;
+    });
+
+    activeEmployeeList = employeeList;
+
+};
+
+
+// Create a new Employee and add it to the database
+const createEmployee = async function(newFirstName, newLastName, newRoleId, newManagerId) {
+    const newRole = await Employee.create({
+        first_name: newFirstName,
+        last_name: newLastName,
+        role_id: newRoleId,
+        manager_id: newManagerId
+
+    })
 }
+
+
 
 
 // Question Arrays ----------------------------------------------------------------------------
@@ -174,7 +207,7 @@ const mainMenuQuestions = [
     {
         type: 'list',
         message: 'What would you like to do? ',
-        choices: ['View All Employees','Add Employees','Update Employee Role','View All Roles','Add Role', 'View All Departments', 'Add Department', 'Quit'],
+        choices: ['View All Employees','Add Employee','Update Employee Role','View All Roles','Add Role', 'View All Departments', 'Add Department', 'Quit'],
         name: 'mainMenuChoice'
     },
     {
@@ -214,17 +247,68 @@ const mainMenuQuestions = [
     {
         type: 'list',
         message: 'Select the department that the role will belong to: ',
-        choices: async () => {await getDepartment(); return activeDepartmentList},
+        choices: async () => {
+            display = false; 
+            await getDepartment(); 
+            display = true; 
+            return activeDepartmentList
+        },
         when: (answers) => { 
             return answers.mainMenuChoice === 'Add Role'
         },
         name: 'roleDepartment'
         
     },
+    {
+        type: 'input',
+        message: 'Enter new Employee first name: ',
+        when: (answers) => { 
+            return answers.mainMenuChoice === 'Add Employee'
+        },
+        name: 'employeeFirstName'
+    },
+    {
+        type: 'input',
+        message: 'Enter new Employee last name: ',
+        when: (answers) => { 
+            return answers.mainMenuChoice === 'Add Employee'
+        },
+        name: 'employeeLastName'
+    },
+    {
+        type: 'list',
+        message: `Select the new employee's role: `,
+        choices: async () => {
+            display = false; 
+            await getRole(); 
+            display = true;
+            return activeRoleList
+        },
+        when: (answers) => { 
+            return answers.mainMenuChoice === 'Add Employee'
+        },
+        name: 'employeeRole'
+        
+    },
+    {
+        type: 'list',
+        message: `Select the new employee's manager: `,
+        choices: async () => {
+            display = false;
+            await getEmployee(); 
+            display = true;
+            return activeEmployeeList
+        },
+        when: (answers) => { 
+            return answers.mainMenuChoice === 'Add Employee'
+        },
+        name: 'employeeManager'
+        
+    },
 
 ];
 
-//syncTables();
+
 const seedData = async function() {
     /*await createDepartment();
     await getDepartment();
@@ -237,7 +321,11 @@ const seedData = async function() {
 }
 
 //seedData();
-askQuestions();
+const init = async function() {
+    syncTables();
+    await askQuestions();
+}
+
 //const test = async ()=>{await getDepartment();console.log(activeDepartmentList);};
 //test();
 
